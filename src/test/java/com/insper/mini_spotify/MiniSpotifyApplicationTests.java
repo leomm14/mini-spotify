a83435292ef67ae88f83f87abc3ae6b8c10bb0e9
leo <leomaxmonte14@gmail.com>
2026-03-15 21:34:28 -0300

    initial commit

---
+++ src/test/java/com/insper/mini_spotify/MiniSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insper.mini_spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
